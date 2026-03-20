7f3d79ed80de9dad09683dbf009efc92db6babef
abhinaysharma-devil <panchalabhinay@gmail.com>
2026-03-20 20:15:19 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { registerRoutes } from "./routes.js";
-import { setupVite, log } from "./vite.js";
+// import { setupVite, log } from "./vite.js";
 import { db } from "./config/firebase.js"; // Import the db instance
 import cors from "cors";
 import { orderRoutes } from "./routes/orderRoutes.js";
@@ -15,12 +15,6 @@ import { paymentRoutes } from "./routes/paymentRoute.js";
 
 const app = express();
 
-// CORS configuration
-// const corsOptions = {
-//   origin: 'http://localhost:5173', // Allow requests from your frontend
-//   optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-//   credentials: true, // Allow cookies to be sent with requests
-// };
 app.use(cors());
 
 app.use(function (req, res, next) {
@@ -78,9 +72,9 @@ app.use((req, res, next) => {
         logLine = logLine.slice(0, 79) + "…";
       }
 
-      log(logLine);
+      // log(logLine);
     } else if (path === "/") {
-      log(`${req.method} === ${path} ${res.statusCode} in ${duration}ms`);
+      // log(`${req.method} === ${path} ${res.statusCode} in ${duration}ms`);
       // res.status(200).send("welcome to the API");
     }
   });
@@ -94,23 +88,9 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-
-console.log("App starting...");
-
 (async () => {
-  console.log("Before registerRoutes");
 
   const server = await registerRoutes(app);
-
-  console.log("After registerRoutes");
-
-  console.log("Before Vite");
-
-  if (process.env.NODE_ENV !== "production") {
-    await setupVite(app, server);
-  }
-
-  console.log("Before listen");
 
   const port = process.env.PORT || 8080;
 
